3ca1a4ae1dfdd7c2b8db27adbc6af524499965a6
Gabriel Falcones <gafalcon@espol.edu.ec>
2022-10-02 22:22:43 -0500

    categories and tags queries

--- graphql/types/Category.ts
+++ graphql/types/Category.ts
@@ -1,9 +1,39 @@
-import { objectType } from "nexus";
+import { objectType, extendType, stringArg, nonNull } from "nexus";
+import { Context } from "../context";
 
 export const Category = objectType({
   name: "Category",
   definition(t) {
     t.int("id");
     t.nonNull.string("value");
+  },
+});
+
+export const CategoriesQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.list.field("categories", {
+      type: Category,
+      async resolve(_, _args, ctx: Context) {
+        return await ctx.prisma.category.findMany();
+      },
+    });
+  },
+});
+
+export const CreateCategoryMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("createCategory", {
+      type: Category,
+      args: {
+        value: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, ctx) {
+        return await ctx.prisma.category.create({
+          data: { value: args.value },
+        });
+      },
+    });
   },
 });
--- graphql/types/Tag.ts
+++ graphql/types/Tag.ts
@@ -1,9 +1,39 @@
-import { objectType } from "nexus";
+import { objectType, extendType, stringArg, nonNull } from "nexus";
+import { Context } from "../context";
 
 export const Tag = objectType({
   name: "Tag",
   definition(t) {
     t.int("id");
     t.nonNull.string("value");
+  },
+});
+
+export const TagsQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.list.field("tags", {
+      type: Tag,
+      async resolve(_, _args, ctx: Context) {
+        return await ctx.prisma.tag.findMany();
+      },
+    });
+  },
+});
+
+export const CreateTagMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("createTag", {
+      type: Tag,
+      args: {
+        value: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, ctx) {
+        return await ctx.prisma.tag.create({
+          data: { value: args.value },
+        });
+      },
+    });
   },
 });
