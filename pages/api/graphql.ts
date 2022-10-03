d286bd1d23777b0ab08339fa5e233615a0aee778
Gabriel Falcones <gafalcon@espol.edu.ec>
2022-10-02 19:06:49 -0500

    configure prisma, restructure graphql

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,27 +1,32 @@
-import { ApolloServer } from "apollo-server-micro"
-import {typeDefs} from "../../server/graphql/schemas"
-import {resolvers} from "../../server/graphql/resolvers"
-import {ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginLandingPageDisabled} from "apollo-server-core"
+import { ApolloServer } from "apollo-server-micro";
+import { typeDefs } from "../../graphql/schemas";
+import { resolvers } from "../../graphql/resolvers";
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled,
+} from "apollo-server-core";
+import { createContext } from "../../graphql/context";
 
 const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [
-        process.env.NODE_ENV === 'production'
-            ? ApolloServerPluginLandingPageDisabled()
-            : ApolloServerPluginLandingPageGraphQLPlayground(),
-    ]
-})
+  typeDefs,
+  resolvers,
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
+  context: createContext,
+});
 
-const startServer = apolloServer.start()
+const startServer = apolloServer.start();
 
 export const config = {
-    api: {
-        bodyParser: false
-    }
-}
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async function handler(req, res) {
-    await startServer;
-    await apolloServer.createHandler({path: "/api/graphql"})(req, res)
+  await startServer;
+  await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
 }
