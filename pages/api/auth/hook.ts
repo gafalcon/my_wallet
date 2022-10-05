c9f68f83cefced069c05b41e5edd6b4244aff694
Gabriel Falcones <gafalcon@espol.edu.ec>
2022-10-04 22:45:31 -0500

    Configure auth0 apis, hook, add to graphql context

---
+++ pages/api/auth/hook.ts
@@ -0,0 +1,27 @@
+import prisma from "../../../lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, name, secret } = req.body;
+  // 1
+  if (req.method !== "POST") {
+    return res.status(403).json({ message: "Method not allowed" });
+  }
+  // 2
+  if (secret !== process.env.AUTH0_HOOK_SECRET) {
+    return res.status(403).json({ message: `You must provide the secret 🤫` });
+  }
+  // 3
+  if (email) {
+    // 4
+    const user = await prisma.user.create({
+      data: { email, name: name || "", total_savings: 0 },
+    });
+    return res.status(200).json({
+      message: `User with email: ${email} has been created successfully!`,
+      userId: user.id,
+    });
+  }
+};
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
+/* import { useUser } from "@auth0/nextjs-auth0"; */
 
 const Home: NextPage = () => {
+  /* const { user } = useUser(); */
+  /* console.log({ user }); */
   return (
     <div>
       <h1 className="text-3xl font-bold underline">My Wallet App</h1>
